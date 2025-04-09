a592227dec4f4e70181e482e6cd65647edfafa17
Dongha Jang <alja34@naver.com>
2025-04-09 16:24:50 +0900

    날씨 앱(진행 중)

---
+++ Clima/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by 장동하 on 4/9/25.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by 장동하 on 4/9/25.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
